7df79bdb7b9d5af751ec5f6d939d862c8b20f1c8
Anis Belkahla <belcalaanis@gmail.com>
2026-04-30 20:09:23 -0400

    oublie d'ajouter le fichier CSS

--- frontend/src/components/modal/supprimer/supprimer.jsx
+++ frontend/src/components/modal/supprimer/supprimer.jsx
@@ -1,13 +1,20 @@
 import { createPortal } from "react-dom";
+import "./supprimer.css";
 
 export default function ModalSupprimer({ annuler, supprimer, nomBuild }) {
   return createPortal(
-    <div>
-      <div>
-        <h1>VVoulez-vous vraiment supprimer le build "{nomBuild}"</h1>
-        <div>
-          <button onClick={annuler}>non</button>
-          <button onClick={supprimer}>Supprimer</button>
+    <div className="modal-overlay">
+      <div className="modal-box">
+        <h1 className="modal-title">
+          Voulez-vous vraiment supprimer le build "{nomBuild}"
+        </h1>
+        <div className="modal-actions">
+          <button className="btn" onClick={annuler}>
+            non
+          </button>
+          <button className="btn" onClick={supprimer}>
+            Supprimer
+          </button>
         </div>
       </div>
     </div>,
